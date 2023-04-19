6d7de78eb59793ab6031feec5f47f163ab055f0e
radamin <radamin@yandex.ru>
2023-04-20 00:32:38 +0300

    Create branch Task_1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,1 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("\nTask 1: ");
+        doTask1(5, 1);
+    }
+
+
+    /*Написать метод, принимающий на вход два аргумента: len и initialValue,
+    и возвращающий одномерный массив типа int длиной len, каждая ячейка которого
+    равна initialValue;
+    */
+
+    public static void doTask1(int len, int initialValue) {
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = initialValue;
+            System.out.print("[" + i + "]" + arr[i] + " ");
+        }
+    }
+}
 
