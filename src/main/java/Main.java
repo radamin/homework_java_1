b5da140f45c58ff3977924df9719a164248159ea
radamin <radamin@yandex.ru>
2023-04-20 00:39:41 +0300

    Create branch Task_2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,1 +1,15 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+public class Main{
+    public static void main(String[] args){
+
+        Integer[] A = { 6, 8, 3, 5, 1, 9 };
+
+        List<Integer> ints = Arrays.asList(A);
+
+        System.out.println("Min element is " + Collections.min(ints));
+        System.out.println("Max element is " + Collections.max(ints));
+    }
+}
